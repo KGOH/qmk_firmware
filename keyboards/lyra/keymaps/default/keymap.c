de1897e63d6634fe7c30374fcac9ba912a4ef2f5
KGOH <kgofhedgehogs@gmail.com>
2020-07-19 20:04:09 +0200

    Fix gesc tild

--- keyboards/lyra/keymaps/default/keymap.c
+++ keyboards/lyra/keymaps/default/keymap.c
@@ -35,13 +35,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTL,  KC_LGUI,  KC_LALT,         KC_BSPC,        TT(_FN1),         KC_SPC,          KC_RALT,   KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT, KC_P0,   KC_PDOT
     ),
     [_FN1] = LAYOUT(
-        KC_TILD, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_PLUS, S(KC_P7),S(KC_P8),S(KC_P9),
+        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_PLUS, S(KC_P7),S(KC_P8),S(KC_P9),
           RGB_MOD,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_UNDS,    KC_INS,     S(KC_P4),S(KC_P5),S(KC_P6),
              _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,   KC_F12,  S(KC_P1),S(KC_P2),S(KC_P3),
         _______,  _______,  _______,         KC_DEL,         _______,          TT(_FN2),        RESET,     KC_PSCR, KC_SLCK, KC_PAUS, KC_RGHT, S(KC_P0),S(KC_PDOT)
    ),
     [_FN2] = LAYOUT(
-        KC_TILD, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_PLUS, S(KC_P7),S(KC_P8),S(KC_P9),
+        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_PLUS, S(KC_P7),S(KC_P8),S(KC_P9),
           RGB_MOD,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_UNDS,    KC_INS,     S(KC_P4),S(KC_P5),S(KC_P6),
              _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,   KC_F12,  S(KC_P1),S(KC_P2),S(KC_P3),
         _______,  _______,  _______,         KC_DEL,         _______,          _______,         RESET,     KC_PSCR, KC_SLCK, KC_PAUS, KC_RGHT, S(KC_P0),S(KC_PDOT)
